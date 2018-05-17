cae5685294c38a93d622d2b7e1ad4c435906b478
Carlos H. Marian <carlosmarian@gmail.com>
2018-05-16 21:37:18 -0300

    Testes com cache de arquivos

--- exemplo/service-worker.js
+++ exemplo/service-worker.js
@@ -1,6 +1,21 @@
 
+let cacheName = 'conhecendoServiceWorker';
+let filesToCache = [
+    '/',
+    'index.html',
+    'styles.css'
+];
+
 self.addEventListener('install', function(e){
     console.log('[SW] Installer');
+    e.waitUntil(
+        caches.open(cacheName)
+        .then(function (cache){
+          console.log('[SW] Caching app shell');
+          //Lista de arquivos que vou add ao cache.
+          return cache.addAll(filesToCache);
+        })
+    );
 });
 
 self.addEventListener('activate', function(e){
@@ -9,4 +24,12 @@ self.addEventListener('activate', function(e){
 
 self.addEventListener('fetch', function(e){
     console.log('[SW] fetch', e.request.url);
+    e.respondWith(
+        caches.match(e.request)
+        .then(function (response){
+            console.log('[SW] Get cache for ', e.request.url )
+            //Se achou a requisição no chache senão executa a requisição
+            return response || fetch(e.request);
+        })
+    );
 });
